5e3eea5f60d5592ade6381ea2019d7cedd559207
marialgoliva <marialgoliva@gmail.com>
2024-02-28 20:22:56 +0100

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,18 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primera tarea",
+    description: "Mi primera tarea",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "Mi primera tarea",
+  },
+  {
+    id: 2,
+    title: "Mi tercera tarea",
+    description: "Mi primera tarea",
+  },
+];
+
